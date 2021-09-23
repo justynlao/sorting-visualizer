8cfc8e2626854a530d05f3c21bb37e6510934f78
jlao1201 <54722681+jlao1201@users.noreply.github.com>
2021-09-22 23:15:53 -0700

    styled navigation bar

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import './Navigation.css'
+import "./Navigation.css";
 
 const Navigation: React.FC = () => {
-  return <nav>
-      <a>Hello</a>
-      <a>Hello</a>
-      <a>Hello</a>
-
-  </nav>;
+  return (
+    <nav>
+      <h1>Sorting Visualizer</h1>
+      <div className="toolbar">
+        <button>Bubble</button>
+        <button>Selection</button>
+        <button>Insertion</button>
+        <button>Merge</button>
+        <button>Quick</button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
