00db1d7dafe9aa1cd38440bb6dd65edaf695ba47
jlao1201 <54722681+jlao1201@users.noreply.github.com>
2021-09-24 22:40:16 -0700

    added logic to animate sorting steps

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,40 @@ import Navigation from "./components/Navigation/Navigation";
 import Array from "./containers/Array/Array";
 import ButtonToolbar from "./components/ButtonToolbar/ButtonToolbar";
 
+import bubbleSort from "./algorithms/Bubble";
+import insertionSort from "./algorithms/Insertion";
+import mergeSort from "./algorithms/Merge";
+import quickSort from "./algorithms/Quick";
+import selectionSort from "./algorithms/Selection";
+
 import "./App.css";
 
 const App: React.FC = () => {
   const [currentAlgo, setCurrentAlgo] = useState("");
   const [array, setArray] = useState<number[]>([]);
-  const [speed, setSpeed] = useState(0);
+  const [compare, setCompare] = useState<number[]>([]); // everytime two elements are being compared, set them to the compared
+  const [swap, setSwap] = useState<number[]>([]); // when two elements are to be swapped, set them to swap
+  const [sorted, setSorted] = useState<number[]>([]); // Whenever an element is sorted, append to the sorted state
+  const [speed, setSpeed] = useState(500);
 
   useEffect(() => {
     resetArray();
-  }, []);
+  }, [currentAlgo]);
 
   const resetArray = () => {
     const arr = [];
-    for (let i = 0; i < 30; i++) {
-      arr.push(Math.floor(Math.random() * 500) + 1);
+    for (let i = 0; i < 10; i++) {
+      arr.push(Math.floor(Math.random() * 500) + 5);
     }
     setArray(arr);
+    setCompare([]);
+    setSwap([]);
+    setSorted([]);
   };
 
-  const handleSortSelection = () => {};
+  const handleSortSelection = (sortAlgo: string) => {
+    setCurrentAlgo(sortAlgo);
+  };
 
   const handleArrayReset = () => {
     resetArray();
@@ -30,12 +44,50 @@ const App: React.FC = () => {
 
   const handleSpeedChange = () => {};
 
-  const handleSort = () => {};
+  const sortHelper = (arr: any) => {
+    for (let i = 0; i < arr.length; i++) {
+      setTimeout(() => {
+        const [first, second, swappedArr, finished] = arr[i];
+        setCompare([first, second]);
+        setSwap([]);
+
+        if (finished !== null) {
+          setSorted((prevState) => [...prevState, finished]);
+        }
+
+        if (swappedArr) {
+          setArray(swappedArr);
+          if (first !== null || second !== null) {
+            setSwap([first, second]);
+          }
+        }
+      }, i * speed);
+    }
+  };
+
+  const handleSort = () => {
+    switch (currentAlgo) {
+      case "Bubble":
+        sortHelper(bubbleSort(array));
+        break;
+      case "Insertion":
+        setArray([...insertionSort(array)]);
+        break;
+      case "Merge":
+        setArray([...mergeSort(array)]);
+        break;
+      case "Quick":
+        setArray([...quickSort(array)]);
+        break;
+      case "Selection":
+        setArray([...selectionSort(array)]);
+    }
+  };
 
   return (
     <div className="App">
       <Navigation onSortSelect={handleSortSelection} />
-      <Array array={array} />
+      <Array array={array} compare={compare} swap={swap} sorted={sorted} />
       <ButtonToolbar
         onArrayReset={handleArrayReset}
         onSpeedChange={handleSpeedChange}
--- src/containers/Array/Array.tsx
+++ src/containers/Array/Array.tsx
@@ -5,13 +5,29 @@ import "./Array.css";
 
 interface ArrayProps {
   array: number[];
+  compare: number[];
+  swap: number[];
+  sorted: number[];
 }
 
-const Array = ({ array }: ArrayProps) => {
+const Array = ({ array, compare, swap, sorted }: ArrayProps) => {
   return (
     <div className="array">
-      {array.map((num) => {
-        return <ArrayBar height={num} />;
+      {array.map((num, index) => {
+        let barColor = 'cornflowerblue';
+        if (compare && (index === compare[0] || index === compare[1])) {
+          barColor = "purple";
+        }
+
+        if (swap && (index === swap[0] || index === swap[1])) {
+          barColor = "red";
+        }
+
+        if (sorted && sorted.includes(index)) {
+          barColor = "green";
+        }
+
+        return <ArrayBar height={num} color={barColor}/>;
       })}
     </div>
   );
