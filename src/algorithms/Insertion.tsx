da540e9c87d3399fe5d87a40037c42f7dad21b58
jlao1201 <54722681+jlao1201@users.noreply.github.com>
2021-09-25 10:54:44 -0700

    added files for other sorting algorithms

---
+++ src/algorithms/Insertion.tsx
@@ -0,0 +1,14 @@
+const insertionSort = (arr: number[]) => {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (arr[j] < arr[j - 1]) {
+        [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+      } else {
+        break;
+      }
+    }
+  }
+  return arr;
+};
+
+export default insertionSort;
