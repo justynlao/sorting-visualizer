1059dd0519d953a2a26f0b33fc0f99fb8c25114b
jlao1201 <54722681+jlao1201@users.noreply.github.com>
2021-09-25 11:53:49 -0700

    implemented insertion sort

--- src/algorithms/Insertion.tsx
+++ src/algorithms/Insertion.tsx
@@ -1,14 +1,23 @@
-const insertionSort = (arr: number[]) => {
+const _ = require("lodash");
+
+const insertionSort = (array: number[]) => {
+  const arr = array.slice();
+  let indices = [];
+
   for (let i = 1; i < arr.length; i++) {
     for (let j = i; j > 0; j--) {
+      indices.push([j, j - 1, null, null]); // Get indices of elements being compared
       if (arr[j] < arr[j - 1]) {
         [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+        indices.push([j, j - 1, arr.slice(), null]); // Get indices of elements being swapped and resulting array
       } else {
         break;
       }
     }
   }
-  return arr;
+
+  indices.push([null, null, null, _.range(arr.length)]); // Get indices of all elements to mark as sorted
+  return indices;
 };
 
 export default insertionSort;
